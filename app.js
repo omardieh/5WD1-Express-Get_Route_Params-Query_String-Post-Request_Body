ac9a03432883f3cfcd5fb4ae4748705583c21869
omardieh <omar.dieh@wildcodeschool.com>
2023-06-24 19:07:08 +0200

    middleware

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
+const { body, validationResult } = require("express-validator");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
+// # Logger : custom middleware
+app.use(activityLogger);
+function activityLogger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+}
 app.get("/", (req, res) => {
-  // console.log(req);
-  // Examples of the "req" methods in the README.md file
-  res.render("index");
+  res.send("Hello, World!");
+});
+app.get("/another-route", (req, res) => {
+  res.send("Hello, another route!");
 });
 
-// app.get("/user-data", (req, res) => {
-//   console.log(req.query);
-//   res.send(req.query);
-// });
-
-app.post("/user-data", (req, res) => {
-  res.send(req.body);
+// # Request Validation
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+const validateRequest = [
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+app.post("/register", validateRequest, (req, res) => {
+  res.send("Process valid registration request");
 });
 
-app.get("/login", (req, res) => res.render("login"));
-app.post("/login", (req, res) => {
-  res.send(req.body);
-});
+// # last Handling Nonexistent Routes
+const errorHandlerMiddleware = (err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+};
+app.use(errorHandlerMiddleware);
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
