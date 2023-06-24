661e354ffaf23e2f77c0587da1f9ae3035aa4425
omardieh <omar.dieh@wildcodeschool.com>
2023-06-24 14:43:24 +0200

    dynamic routes with params

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
+  // Examples of the "req" methods in the README.md file
+});
+
+// dynamic route
+app.get("/users/:username", (req, res) => {
+  res.send(req.params);
+});
+
+// nested dynamic routes
+app.get("/users/:username/books/:bookID", (req, res) => {
+  res.send(req.params);
+});
+
+// Query String
+app.get("/search", (req, res, next) => {
+  res.send(req.query);
 });
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
